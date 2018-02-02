b46337a3dc24bcd0ed28300ed3ae8c64fc043f0d
Jeremy Vanpé <jeremy.vanpe@ehealth.fgov.be>
2018-02-02 16:31:31 +0100

    Add logback configuration

--- src/main/java/be/jeremy/manager/expense/repository/AccountRepository.java
+++ src/main/java/be/jeremy/manager/expense/repository/AccountRepository.java
@@ -1,6 +1,8 @@
 package be.jeremy.manager.expense.repository;
 
 import be.jeremy.manager.expense.domain.Account;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
@@ -12,11 +14,15 @@ import java.util.Map;
 @Repository
 public class AccountRepository {
 
+    private static final Logger log = LoggerFactory.getLogger(AccountRepository.class);
+
     public static Map<Long, Account> fakeRepository = new HashMap<Long, Account>(){{
         put(1L, new Account(1L, "MyAccount", "This is my account"));
     }};
 
     public Account getAccount(Long id) {
+        log.info("Entering int getAccount...");
+
         return fakeRepository.get(id);
     }
 }
