5cf3ce7ce8ae80d51137342f3cf88defd49a3a60
Jeremy Vanpé <jeremy.vanpe@ehealth.fgov.be>
2018-02-15 17:46:56 +0100

    Add aspect for logging.

---
+++ src/main/java/be/jeremy/manager/expense/config/LoggingAspectConfiguration.java
@@ -0,0 +1,20 @@
+package be.jeremy.manager.expense.config;
+
+import be.jeremy.manager.expense.aop.logging.LoggingAspect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.Environment;
+
+/**
+ * @author Jeremy
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class LoggingAspectConfiguration {
+
+    @Bean
+    public LoggingAspect loggingAspect(Environment env) {
+        return new LoggingAspect();
+    }
+}
